a4f578fbb553c310619c3bdb820608eda290f71f
s18844 <s18844@pjwstk.edu.pl>
2020-03-25 13:38:54 +0100

    First Commit
    First Commit

---
+++ Cwiczenia2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+
+namespace Cwiczenia2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int blokada = 0;
+            List<Studenci.student> ListaStudent = new List<Studenci.student>();
+            using (var stream = new StreamReader(@"C:\Users\Postek\Desktop\dane.csv"))
+            {
+                string line = null;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    string[] student = line.Split(',');
+                    int czyWszystkieKolumny=student.Length;
+                    Boolean czyJestPustaWartosc = false;
+                    Boolean CzyTakiStudentJuzJest = false;
+                    for (int i = 0; i < student.Length; i++)
+                    {
+                        if (student[i].Length == 0) { czyJestPustaWartosc = true; break; }
+                    }
+                    if (czyWszystkieKolumny == 9 && czyJestPustaWartosc==false)
+                    {
+                        var stx = new Studenci.student.studies
+                        {
+                            PrzedmiotStudiowania = student[2],
+                            TypStudiow = student[3]
+                        };
+
+                        var st = new Studenci.student
+                        {
+                            Imie = student[0],
+                            Nazwisko = student[1],
+                            StudiesObiekt = stx,
+                            SKA = student[4],
+                            Data = student[5],
+                            Mail = student[6],
+                            ImieMamy = student[7],
+                            ImieTaty = student[8]
+                        };
+                        st.Nazwisko = Regex.Replace(st.Nazwisko, @"[\d-]", string.Empty);
+                        
+                        foreach (var StudentZListy in ListaStudent)
+                        {
+                     
+                            if (StudentZListy.Imie == st.Imie && StudentZListy.Nazwisko == st.Nazwisko && StudentZListy.SKA == st.SKA)
+                            {
+                                CzyTakiStudentJuzJest = true;
+                                break;
+                            }
+                        }
+                        if(CzyTakiStudentJuzJest==false)ListaStudent.Add(st);
+                      /*  Console.WriteLine("Imie:" + st.Imie + " Nazwisko:" + st.Nazwisko + " PrzedmiotStudiowania:"
+                            + st.PrzedmiotStudiowania + " TypStudiow:" + st.TypStudiow
+                            + " SKA:" + st.SKA + " Data:" + st.Data + " Mail:" + st.Mail
+                            + " ImieMamy:" + st.ImieMamy + " ImieTaty:" + st.ImieTaty);
+                    */
+                    }
+                    if (czyJestPustaWartosc==true || CzyTakiStudentJuzJest==true)
+                    {
+                        String zapiszLogDoPliku = "";
+                        for (int i = 0; i < student.Length; i++)
+                        {
+                            zapiszLogDoPliku += student[i]+" ";
+                            if (i++ == student.Length) zapiszLogDoPliku += ",";
+                        }
+
+                        File.AppendAllText(@"C:\Users\Postek\Desktop\LogError.txt", zapiszLogDoPliku + Environment.NewLine);
+
+                    }
+                   
+                }
+            }
+            Console.WriteLine(ListaStudent.Count);
+
+            List<Studenci> ListaSt = new List<Studenci>();
+            foreach (var StudentZListy in ListaStudent)
+            {
+
+                var wynik = new Studenci();
+                wynik.StudentObiekt = StudentZListy;
+                ListaSt.Add(wynik);
+
+                            }
+
+            FileStream writer = new FileStream(@"C:\Users\Postek\Desktop\data.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Studenci>), new XmlRootAttribute("uczelnia"));
+            
+            serializer.Serialize(writer, ListaSt);
+
+
+
+        }
+    }
+}
---
+++ Cwiczenia2/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Cwiczenia2
+{
+    [Serializable]
+    public class Studenci
+    {
+        [XmlElement(ElementName = "student")]
+        public student StudentObiekt;
+        
+        public class student
+        {
+            [XmlAttribute(AttributeName = "indexNumber")]
+            public string SKA { get; set; }
+            [XmlElement(ElementName = "fname")]
+            public string Imie { get; set; }
+            [XmlElement(ElementName = "lname")]
+            public string Nazwisko { get; set; }
+
+            public class studies
+            {
+                [XmlElement(ElementName = "name")]
+                public string PrzedmiotStudiowania { get; set; }
+                [XmlElement(ElementName = "mode")]
+                public string TypStudiow { get; set; }
+            }
+
+            [XmlElement(ElementName = "birthdate")]
+            public string Data { get; set; }
+            [XmlElement(ElementName = "email")]
+            public string Mail { get; set; }
+            [XmlElement(ElementName = "mothersName")]
+            public string ImieMamy { get; set; }
+            [XmlElement(ElementName = "fathersName")]
+            public string ImieTaty { get; set; }
+            [XmlElement(ElementName = "studies")]
+            public student.studies StudiesObiekt;
+        }
+
+      
+    }
+}
